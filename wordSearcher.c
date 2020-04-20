b773a79995ee639a2080b55f94357263a3cd7702
Kürşat Erdoğan <31738335+kursaterdogan@users.noreply.github.com>
2020-04-20 20:10:32 +0300

    Update wordSearcher.c

--- wordSearcher.c
+++ wordSearcher.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-    char string [100];
+    char string[100];
     char word[100];
-    int a=0,b=0,i,j,k,c,d=0;
+    int a = 0, b = 0, i, j, k, c, d = 0;
 
 
     printf("Please enter the sentence\n");
@@ -14,38 +14,39 @@ int main()
     printf("Please enter the word\n");
     gets(word);
 
-    while (string[a]!='\0')
+    while (string[a] != '\0')
     {
         a++;
     }
 
-     while (word[b]!='\0')
+    while (word[b] != '\0')
     {
         b++;
     }
 
-    for(i=0;i<a;i++)
+    for (i = 0; i < a; i++)
     {
-        k=i;
-        c=0;
-        for(j=0;j<b;j++)
+        k = i;
+        c = 0;
+        for (j = 0; j < b; j++)
         {
-            if(string[k]==word[j])
+            if (string[k] == word[j])
             {
-                c++;k++;
+                c++;
+                k++;
 
             }
         }
-         if (c==j)
-            {
+        if (c == j)
+        {
             printf("Sentence contains that word\n");
-            printf("[%d-%d] \n",k-j+1,k);
+            printf("[%d-%d] \n", k - j + 1, k);
             d++;
-            }
+        }
 
     }
-    if (d==0)
-            printf("Sentence does not contain that word\n");
+    if (d == 0)
+        printf("Sentence does not contain that word\n");
 
     return 0;
 }
